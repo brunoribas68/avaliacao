a5560647fcc82d9ddf4bd34b7c4c44f89c11f80e
brunoribas68 <brunoribas68@users.noreply.github.com>
2017-11-30 11:23:35 -0200

    Update index.php

--- view/index.php
+++ view/index.php
@@ -1,13 +1,3 @@
 <?php
-
-require_once  '../model/clientes_db.php';
-
-$mostra = new Conexao;
-$result = $mostra->selectClientes('Bruno');
-echo $result;
-/*foreach($result as $linha){
-	echo $linha['telefone'];
-}*/
-
-
+header("Location: http://localhost/view/clientes.html");
 ?>
